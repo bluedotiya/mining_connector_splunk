e2994b602cc266cccd15614416bb445aef0efd15
bluedotiya <75704012+bluedotiya@users.noreply.github.com>
2021-05-30 14:33:00 +0300

    Update eth_price_watch.sh

--- helper_scripts/eth_price_watch.sh
+++ helper_scripts/eth_price_watch.sh
@@ -3,9 +3,10 @@
 #This script gets ETHUSDT Price from Binance API
 
 #Constants
+output_path="/var/tmp"
 eth_price=$(curl -X GET https://api.binance.com/api/v3/avgPrice?symbol=ETHUSDT  2> /dev/null | cut -d : -f3 | cut -d \" -f2)
 trimmed_price=${eth_price:0:8}
 
 
-echo $trimmed_price > /var/tmp/stats_eth_price
+echo $trimmed_price > $output_path/stats_eth_price
 echo $trimmed_price
