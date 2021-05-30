f3d4a2aa5978e640383ef0958c9f89fa4015b6bc
bluedotiya <75704012+bluedotiya@users.noreply.github.com>
2021-05-30 14:31:51 +0300

    Update wei_unpaid_balance.sh
    changed data output into configurable variable

--- helper_scripts/wei_unpaid_balance.sh
+++ helper_scripts/wei_unpaid_balance.sh
@@ -4,8 +4,8 @@
 
 #Constant
 wallet_address=""
-
+output_path="/var/tmp"
 wei_unpaid=$(curl -X GET "https://flexpool.io/api/v1/miner/$wallet_address/balance/" | cut -d : -f3 | cut -d \} -f1)
 
 
-echo $wei_unpaid > /var/tmp/stats_wei_unpaid
+echo $wei_unpaid > $output_path/stats_wei_unpaid
