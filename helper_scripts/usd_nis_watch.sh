5ab4c9b3311df3ebe8aa992a28b8d38536e085e6
bluedotiya <75704012+bluedotiya@users.noreply.github.com>
2021-05-30 14:33:28 +0300

    Update usd_nis_watch.sh

--- helper_scripts/usd_nis_watch.sh
+++ helper_scripts/usd_nis_watch.sh
@@ -3,7 +3,8 @@
 #Get the currency ratio between the USD (American dollar) and NIS (Israeli new shekel)
 
 #Constant
+output_path="/var/tmp"
 usd_to_nis=$(curl https://www.boi.org.il/en/Markets/ExchangeRates/Pages/Default.aspx 2> /dev/null | grep -A 1 "<td class=\"BoiExchangeRateCountry\">USA</td>" | grep "\."  | cut -d \> -f2 | cut -d \< -f1)
 
 
-echo $usd_to_nis > /var/tmp/stats_usd_to_nis
+echo $usd_to_nis > $output_file/stats_usd_to_nis
