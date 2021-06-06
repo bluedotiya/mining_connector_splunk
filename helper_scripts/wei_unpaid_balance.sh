b804eb90650da7c523e5009613f60695f35f3649
bluedotiya <75704012+bluedotiya@users.noreply.github.com>
2021-06-06 19:23:57 +0300

    Updated helper script to Flexpool API V2

--- helper_scripts/wei_unpaid_balance.sh
+++ helper_scripts/wei_unpaid_balance.sh
@@ -5,7 +5,7 @@
 #Constant
 wallet_address=""
 output_path="/var/tmp"
-wei_unpaid=$(curl -X 'GET'   'https://api.flexpool.io/v2/miner/balance?coin=eth&address=$wallet_address&countervalue=USD' | cut -d : -f4 | cut -d , -f1)
+wei_unpaid=$(curl -X 'GET'   "https://api.flexpool.io/v2/miner/balance?coin=eth&address=$wallet_address&countervalue=USD" | cut -d : -f4 | cut -d , -f1)
 
 
-echo $wei_unpaid > $output_path/stats_wei_unpaid
+echo "$wei_unpaid" > $output_path/stats_wei_unpaid
